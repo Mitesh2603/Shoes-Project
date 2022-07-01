d41d5e993bd562f32c3e7385052cddb83c24b199
Mitesh Makwana <mitesh.makwana@exhibytesolution.com>
2022-07-01 12:29:04 +0530

    Task # Completed

---
+++ src/components/child.vue
@@ -0,0 +1,25 @@
+<template>
+  <center>
+    <h1 class="text-2xl font-serif">{{ message }}</h1><br />
+    <app-input
+      class="font-serif"
+      :msg="message"
+      @messageChanged="message = $event"
+    ></app-input><br/>
+  </center>
+</template>
+
+<script>
+import Parent from "./parent.vue";
+
+export default {
+  data() {
+    return {
+      message: "My name is Mitesh!",
+    };
+  },
+  components: {
+    "app-input": Parent,
+  },
+};
+</script>
---
+++ src/components/parent.vue
@@ -0,0 +1,28 @@
+<template>
+  <center>
+    <input
+      class="border-solid border-2 border-black font-serif"
+      type="text"
+      :value="msg"
+      @input="changeMessage"
+    />
+    <p>{{ message }}</p>
+  </center>
+</template>
+
+<script>
+export default {
+  props: ["msg"],
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    changeMessage(event) {
+      this.message = event.target.value;
+      this.$emit("messageChanged", this.message);
+    },
+  },
+};
+</script>
