d3f20a2ef20ae6db6fc9fb5ba14ff4a6487bf121
Mitesh Makwana <mitesh.makwana@exhibytesolution.com>
2022-07-27 19:57:55 +0530

    Actions & Modules

--- src/components/CompB.vue
+++ src/components/CompB.vue
@@ -8,14 +8,24 @@ export default {
       store,
     };
   },
+  // computed: {
+  //   count() {
+  //     return this.$store.state.count;
+  //   },
+  // },
   methods: {
-    ...mapMutations(["increment"]),
+    reduceCount() {
+      this.$store.commit("reduceCount");
+    },
   },
 };
 </script>
 
 <template>
   <div>
+    <!-- <h3>This is count : {{ count }}</h3>
+    <button @click="reduceCount">Reduce Count</button>
+    <br /><br /> -->
     <button @click="store.commit('increment')">
       From B Increment 1 : {{ store.state.count }}</button
     ><br /><br />
--- src/components/store.js
+++ src/components/store.js
@@ -1,13 +1,9 @@
 import { createStore } from "vuex";
 import { SOME_MUTATION } from "./mutation-types";
 
-export const store = createStore({
+const moduleA = {
   state: {
     count: 0,
-    todos: [
-      { id: 1, text: "...", done: true },
-      { id: 2, text: "...", done: false },
-    ],
   },
   mutations: {
     increment(state) {
@@ -19,6 +15,31 @@ export const store = createStore({
     [SOME_MUTATION](state) {
       state.count++;
     },
+    reduceCount: (state) => {
+      setTimeout(function () {
+        state.count -= count;
+      }, 3000);
+    },
+  },
+  actions: {
+    increment(state) {
+      state.commit("increment");
+    },
+    incrementIfOddOnRootSum({ state, commit, rootState }) {
+      if ((state.count + rootState.count) % 2 === 1) {
+        commit("increment");
+      }
+    },
+  },
+};
+
+const moduleB = {
+  state: {
+    count: 0,
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+    ],
   },
   getters: {
     donoTodos(state) {
@@ -26,8 +47,31 @@ export const store = createStore({
     },
   },
   actions: {
-    increment(context) {
-      context.commit("increment");
+    actionA({ commit }) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit("increment");
+          resolve();
+        }, 1000);
+      });
+    },
+    reduceCount({ commit }) {
+      setTimeout(function () {
+        commit("reduceCount");
+      }, 3000);
     },
   },
+};
+
+export const store = createStore({
+  modules: {
+    a: moduleA,
+    b: moduleB,
+  },
 });
+
+store.state.a;
+store.state.b;
+store.commit("increment");
+// store.dispatch('actionA').then(() => {})
+store.dispatch("reduceCount");
