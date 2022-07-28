da7a2311a0f82caecd0911bf34c9dbe7f5d2ec7e
Mitesh Makwana <mitesh.makwana@exhibytesolution.com>
2022-07-28 19:48:48 +0530

    Modules

--- src/components/store.js
+++ src/components/store.js
@@ -34,6 +34,36 @@ const moduleA = {
 };
 
 const moduleB = {
+  foo: {
+    namespaced: true,
+
+    getters: {
+      someGetter(state, getters, rootState, rootGetters) {
+        getters.someOtherGetter; // -> 'foo/someOtherGetter'
+        rootGetters.someOtherGetter; // -> 'someOtherGetter'
+        rootGetters["bar/someOtherGetter"]; // -> 'bar/someOtherGetter'
+      },
+      someOtherGetter: (state) => {
+        "...";
+      },
+    },
+    actions: {
+      someAction({ dispatch, commit, getters, rootGetters }) {
+        getters.someGetter; // -> 'foo/someGetter'
+        rootGetters.someGetter; // -> 'someGetter'
+        rootGetters["bar/someGetter"]; // -> 'bar/someGetter'
+
+        dispatch("someOtherAction"); // -> 'foo/someOtherAction'
+        dispatch("someOtherAction", null, { root: true }); // -> 'someOtherAction'
+
+        commit("someMutation"); // -> 'foo/someMutation'
+        commit("someMutation", null, { root: true }); // -> 'someMutation'
+      },
+      someOtherAction(ctx, payload) {
+        "...";
+      },
+    },
+  },
   state: {
     count: 0,
     todos: [
